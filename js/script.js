f1109d4525010926ee57105407e685e4df35e00a
Jeniffer-Maina <mainajeniffer221@gmail.com>
2025-06-01 15:43:43 +0000

    project

--- js/script.js
+++ js/script.js
@@ -52,3 +52,22 @@ document.addEventListener("DOMContentLoaded", function () {
         showCard(current);
     });
 });
+
+
+document.addEventListener("scroll", function() {
+  const sections = document.querySelectorAll("section[id]");
+  const navLinks = document.querySelectorAll(".navbar a");
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 60;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
